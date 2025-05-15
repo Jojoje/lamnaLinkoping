e0af831db951af03c8170430b4c868c05b367648
Joakim Nilsson <jojoje.nilsson@gmail.com>
2025-05-15 17:52:24 +0200

    chore: seperate file for info

---
+++ app/faqInfo.tsx
@@ -0,0 +1,46 @@
+import { FAQItem } from "./faq";
+
+export const linkopingFaqData: FAQItem[] = [
+  {
+    question: 'Men va? Varför ska du flytta?!',
+    answer: 'Our return policy lasts 30 days. If 30 days have gone by since your purchase, unfortunately we can’t offer you a refund or exchange.',
+  },
+  {
+    question: 'How long does shipping take?',
+    answer: 'Shipping usually takes 5–7 business days depending on your location.',
+  },
+  {
+    question: 'Do you ship internationally?',
+    answer: 'Yes, we ship to most countries worldwide. Shipping fees and delivery times vary by destination.',
+  },
+];
+
+export const partyFaqData: FAQItem[] = [
+  {
+    question: 'Får man säga hejdå på typ ett event?',
+    answer: 'Our return policy lasts 30 days. If 30 days have gone by since your purchase, unfortunately we can’t offer you a refund or exchange.',
+  },
+  {
+    question: 'How long does shipping take?',
+    answer: 'Shipping usually takes 5–7 business days depending on your location.',
+  },
+  {
+    question: 'Do you ship internationally?',
+    answer: 'Yes, we ship to most countries worldwide. Shipping fees and delivery times vary by destination.',
+  },
+];
+
+export const falunFaqData: FAQItem[] = [
+  {
+    question: 'Men vart flyttar du?',
+    answer: 'Our return policy lasts 30 days. If 30 days have gone by since your purchase, unfortunately we can’t offer you a refund or exchange.',
+  },
+  {
+    question: 'How long does shipping take?',
+    answer: 'Shipping usually takes 5–7 business days depending on your location.',
+  },
+  {
+    question: 'Do you ship internationally?',
+    answer: 'Yes, we ship to most countries worldwide. Shipping fees and delivery times vary by destination.',
+  },
+];
